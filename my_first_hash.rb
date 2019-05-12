c4b0bf721cdccc8b3d36be25d9efa8b5740330fe
Haley DeKeyser <hcd09@live.com>
2019-05-12 14:36:58 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -5,9 +5,9 @@ end
 
 def shipping_manifest
   the_manifest = { "whale bone corsets" => 5, 
-    "porcelain vases" => 2, 
-    "oil paintings" => 3
-  }
+                  "porcelain vases" => 2, 
+                  "oil paintings" => 3
+                }
 end
 
 
@@ -17,9 +17,7 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+  shipping_manifest["oil paintings"]
 end
 
 def adding
